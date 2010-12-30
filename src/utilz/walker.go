68b82fb38159a20784f228298a6d1176aefbe11c
Bjarne Holen <bjarneholen@gmail.com>
2010-12-30 21:15:40 +0100

    minor: removed some out-commented code

--- src/utilz/walker.go
+++ src/utilz/walker.go
@@ -6,7 +6,6 @@ package walker /* texas ranger */
 
 import (
     "os"
-///     "container/vector"
     "path"
 )
 
@@ -22,13 +21,11 @@ var IncludeDir = func(p string) bool { return true }
 var IncludeFile = func(p string) bool { return true }
 
 type collect struct {
-///     Files *vector.StringVector
     Files []string
 }
 
 func newCollect() *collect {
     c := new(collect)
-///     c.Files = new(vector.StringVector)
     c.Files = make([]string, 0)
     return c
 }
@@ -39,12 +36,10 @@ func (c *collect) VisitDir(path string, d *os.FileInfo) bool {
 
 func (c *collect) VisitFile(path string, d *os.FileInfo) {
     if IncludeFile(path) {
-///         c.Files.Push(path)
         c.Files = append(c.Files, path)
     }
 }
 
-/// func PathWalk(root string) *vector.StringVector {
 func PathWalk(root string) []string {
     c := newCollect()
     errs := make(chan os.Error)
